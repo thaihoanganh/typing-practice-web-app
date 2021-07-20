e04790d8a9b662b8bd56ac82eef5dada30fdb787
thaihoanganh <hoanganh0811999@gmail.com>
2021-07-20 22:31:21 +0700

    feat(organisms): add new TextData component

---
+++ src/components/organisms/TextData/TextDataWrapper.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Grid from "@/components/atoms/Grid";
+
+export const TextDataWrapper: React.FC = ({ children }) => {
+  return (
+    <Grid style={{ height: 200 }} className="overflow-hidden relative m-lg border rounded" bordered>
+      {children}
+    </Grid>
+  );
+};
+
+export default TextDataWrapper;
---
+++ src/components/organisms/TextData/index.ts
@@ -0,0 +1,4 @@
+export { default } from "./TextData";
+export * from "./TextData";
+export * from "./TextDataWrapper";
+export * from "./TextDataInput";
