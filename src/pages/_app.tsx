80dbbb1c0eba895e4fa53a32f44bb20e07341292
thaihoanganh <hoanganh0811999@gmail.com>
2021-07-26 15:21:44 +0700

    refactor(pages): refactor setting module

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import "../../public/styles/global.css";
 import type { AppProps } from "next/app";
 
 import { UserProvider } from "@/modules/user/providers";
+import { SettingProvider } from "@/modules/setting/providers";
 import MainLayout from "@/components/templates/MainLayout";
 import Header from "@/containers/Header";
 
 const AppWrapper = ({ Component, pageProps }: AppProps) => {
   return (
     <UserProvider>
-      <MainLayout header={<Header />} content={<Component {...pageProps} />} />
+      <SettingProvider>
+        <MainLayout header={<Header />} content={<Component {...pageProps} />} />
+      </SettingProvider>
     </UserProvider>
   );
 };
