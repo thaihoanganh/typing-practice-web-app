bd3989de271d3aca27d7a1fd831ae021a770c2f2
thaihoanganh <hoanganh0811999@gmail.com>
2021-07-31 00:16:23 +0700

    refactor(TextData-component): update TextData component

--- src/components/organisms/TextData/TextDataInput.tsx
+++ src/components/organisms/TextData/TextDataInput.tsx
@@ -1,27 +1,59 @@
-import React from "react";
-import classNames from "classnames";
+import React, { useRef, useEffect } from "react";
 
 import Grid from "@/components/atoms/Grid";
+import Text from "@/components/atoms/Text";
 
 export interface TextDataInputProps {
+  cursonWord: number;
   showDraft?: boolean;
+  getValue?: (value: string) => void;
   onHandleKeyPress: (characterCode: number) => void;
   onToggleFocus: (isFocus: boolean) => void;
 }
 
-export const TextDataInput: React.FC<TextDataInputProps> = ({ showDraft, onHandleKeyPress, onToggleFocus }) => {
+export const TextDataInput: React.FC<TextDataInputProps> = ({
+  cursonWord,
+  showDraft,
+  getValue,
+  onHandleKeyPress,
+  onToggleFocus,
+}) => {
+  const ref: any = useRef(null);
+
+  useEffect(() => {
+    if (cursonWord) {
+      ref.current.innerText = "";
+    }
+  }, [cursonWord]);
+
+  const onHandleInput = (e: any) => {
+    onHandleKeyPress(e.charCode);
+    if (getValue && ref.current) {
+      getValue(ref.current.innerText);
+    }
+  };
+
   return (
-    <label className="absolute flex items-end w-full h-full cursor-text">
-      <Grid className={classNames("w-full py-sm bg-primary text-center", showDraft && "border-t")} bordered>
-        <input
-          type="text"
-          className={classNames("focus:outline-none text-center text-headline-2", !showDraft && "opacity-0")}
-          onFocus={() => onToggleFocus(true)}
-          onBlur={() => onToggleFocus(false)}
-          onKeyPress={(event) => onHandleKeyPress(event.charCode)}
-        />
-      </Grid>
-    </label>
+    <React.Fragment>
+      {ref.current && showDraft && (
+        <Grid
+          className="absolute bottom-0 flex items-center justify-center w-full h-12 border-t"
+          color="primary"
+          bordered
+        >
+          <Text variant="headline-1">{ref.current.innerText}</Text>
+        </Grid>
+      )}
+      <div
+        ref={ref}
+        contentEditable
+        className={"overflow-hidden absolute inset-0 opacity-0"}
+        onFocus={() => onToggleFocus(true)}
+        onBlur={() => onToggleFocus(false)}
+        onPaste={(e) => e.preventDefault()}
+        onKeyPress={onHandleInput}
+      />
+    </React.Fragment>
   );
 };
 
