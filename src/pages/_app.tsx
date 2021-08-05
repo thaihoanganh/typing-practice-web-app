7b2cf240f9ca84036fdad74a24cdc4b9e4d1d94d
thaihoanganh <hoanganh0811999@gmail.com>
2021-08-05 15:23:39 +0700

    feat(_app): implement MainLayout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../../public/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../../public/styles/globals.css";
+import React from "react";
+import type { AppProps } from "next/app";
+
+import MainLayout from "@/components/templates/MainLayout";
+import Header from "@/containers/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <MainLayout header={<Header />} content={<Component {...pageProps} />} />
+    </React.Fragment>
+  );
 }
-export default MyApp
+export default MyApp;
