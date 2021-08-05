818dca02c9c16d678838f9f84c42644ab5a5e519
thaihoanganh <hoanganh0811999@gmail.com>
2021-08-05 15:19:22 +0700

    fix(MainLayout): fix height style

--- src/components/templates/MainLayout/MainLayout.tsx
+++ src/components/templates/MainLayout/MainLayout.tsx
@@ -10,7 +10,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({ header, content }) => {
   return (
     <div className="min-h-screen bg-secondary">
       <div className={classes.header}>
-        <div className="desktop:w-960 mx-auto">{header}</div>
+        <div className="desktop:w-960 h-full mx-auto">{header}</div>
       </div>
       <div className={classes.content}>
         <div className="desktop:w-960 mx-auto">{content}</div>
