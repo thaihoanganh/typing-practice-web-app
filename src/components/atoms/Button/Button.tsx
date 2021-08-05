e44f41ec0c3a8fb2ccb542d40d935413c45c52e3
thaihoanganh <hoanganh0811999@gmail.com>
2021-08-05 14:14:00 +0700

    feat(atoms): create new Button component

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,54 @@
+import React, { forwardRef } from "react";
+import classNames from "classnames";
+
+const COLORS = {
+  primary: [
+    "bg-primary disabled:bg-opacity-32",
+    "bg-contrast-primary bg-opacity-0 hover:bg-opacity-8 active:bg-opacity-32",
+    "text-contrast-primary",
+  ],
+  secondary: [
+    "bg-contrast-secondary bg-opacity-8",
+    "bg-contrast-secondary bg-opacity-0 hover:bg-opacity-8 active:bg-opacity-32",
+    "text-contrast-secondary",
+  ],
+};
+
+export interface ButtonProps
+  extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  color?: keyof typeof COLORS;
+  fullWitdh?: boolean;
+}
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, color = "primary", disabled, fullWitdh, ...otherProps }, refButton) => {
+    return (
+      <button
+        className={classNames(
+          "overflow-hidden relative rounded px-md py-sm disabled:pointer-events-none",
+          COLORS[color][0],
+          className,
+          fullWitdh && "w-full"
+        )}
+        disabled={disabled}
+        ref={refButton}
+        {...otherProps}
+      >
+        <div className={classNames("absolute inset-0", COLORS[color][1])} />
+        <span
+          className={classNames(
+            "text-button font-medium",
+            COLORS[color][2],
+            disabled && color === "secondary" && "text-opacity-32"
+          )}
+        >
+          {children}
+        </span>
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export default Button;
