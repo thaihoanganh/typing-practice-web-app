073623844162939e5d66e7d287b1982198389f57
thaihoanganh <hoanganh0811999@gmail.com>
2021-08-13 18:07:21 +0700

    feat(containers): implement SpeedTest container

---
+++ src/containers/SpeedTest/SpeedTestStatistical.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const SpeedTestStatistical = () => {
+  return <div></div>;
+};
+
+export default SpeedTestStatistical;
---
+++ src/containers/SpeedTest/index.ts
@@ -0,0 +1,3 @@
+export * from "./SpeedTestPractice";
+export * from "./SpeedTestControl";
+export * from "./SpeedTestStatistical";
