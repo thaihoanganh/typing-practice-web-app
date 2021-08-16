811e0672a8cb0a6ad26d96c0096e3cbddb8720b8
thaihoanganh <hoanganh0811999@gmail.com>
2021-08-16 17:44:04 +0700

    refactor(setting-container): refactor settings module

--- src/containers/Setting/SoundSetting.tsx
+++ src/containers/Setting/SoundSetting.tsx
@@ -1,40 +1,7 @@
 import React from "react";
-import { useSetting, actionUpdateSetting } from "@/modules/setting";
-import Button from "@/components/atoms/Button";
 
-export const SoundSetting: React.FC = () => {
-  const { setting } = useSetting();
-  const { options, selected } = setting.sound;
-
-  const onToggleSound = (soundSelected: number) => {
-    actionUpdateSetting({
-      ...setting,
-      sound: {
-        ...setting.sound,
-        selected: soundSelected,
-      },
-    });
-  };
-
-  return (
-    <React.Fragment>
-      <div className="text-headline-2 text-contrast-secondary">Âm thanh</div>
-      <div className="p-md">
-        <div className="flex">
-          {options.map((option, index) => (
-            <div className="mr-md" key={index}>
-              <Button
-                color={index === selected ? "primary" : "secondary"}
-                onClick={() => onToggleSound(index)}
-              >
-                {option.name}
-              </Button>
-            </div>
-          ))}
-        </div>
-      </div>
-    </React.Fragment>
-  );
+export const SoundSetting = () => {
+  return <div></div>;
 };
 
 export default SoundSetting;
