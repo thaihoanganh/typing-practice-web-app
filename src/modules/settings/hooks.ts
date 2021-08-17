3f86b3eb567ca2aa13e0890ead2db608b72e8017
thaihoanganh <hoanganh0811999@gmail.com>
2021-08-17 20:30:58 +0700

    refactor(settings-module): update useSettings

--- src/modules/settings/hooks.ts
+++ src/modules/settings/hooks.ts
@@ -1,12 +1,12 @@
 import { useContext } from "react";
 import { SettingContext } from ".";
 
-export const useSetting = () => {
-  const { status, errorMessage, entity } = useContext(SettingContext.initial);
+export const useSettings = () => {
+  const { status, message, entity } = useContext(SettingContext.initial);
 
   return {
-    settingStatus: status,
-    settingErrorMessage: errorMessage,
-    setting: entity,
+    settingsStatus: status,
+    settingsMessage: message,
+    settings: entity,
   };
 };
