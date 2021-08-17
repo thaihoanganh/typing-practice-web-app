7c1f659dd1d80774fe677312e7b9292ba9131f02
thaihoanganh <hoanganh0811999@gmail.com>
2021-08-17 20:30:31 +0700

    refactor(settings-module): implement theme setting

--- src/modules/settings/actions.ts
+++ src/modules/settings/actions.ts
@@ -1,4 +1,5 @@
 import produce from "immer";
+import { v4 as uuidv4 } from "uuid";
 import { SettingContext, initialSettings, settingSelectSchema, settingOptionsSchema } from ".";
 
 export async function actionResetSettings() {
@@ -19,7 +20,7 @@ export async function actionResetSettings() {
   SettingContext.setState((prevState) => ({
     ...prevState,
     status: "READY",
-    errorMessage: null,
+    message: null,
     entity: initialSettings,
   }));
 }
@@ -44,7 +45,7 @@ export async function actionGetSettings() {
           settingSelect.map((select) => {
             const settingName: keyof typeof initialSettings = select.settingName;
             const options: any = settingOptions?.map((option) => {
-              if (settingName === option.settingName) {
+              if (option._id && settingName === option.settingName) {
                 return {
                   _id: option._id,
                   name: option.name,
@@ -55,8 +56,13 @@ export async function actionGetSettings() {
             });
 
             draft.status = "READY";
-            draft.entity[settingName].options = [...draft.entity[settingName].options, ...options];
             draft.entity[settingName].selected = select.value;
+            if (options[0]) {
+              draft.entity[settingName].options = [
+                ...draft.entity[settingName].options,
+                ...options,
+              ];
+            }
           });
         })
       );
@@ -68,31 +74,183 @@ export async function actionGetSettings() {
   }
 }
 
-export async function actionChangeSound(soundSelect: string) {
+export async function actionToggleSetting(settingName: string, selected: string) {
   const { status, entity, storage } = SettingContext.getState();
 
   if (status === "READY") {
-    const validSoundSelect = entity.sound.options.find((option) => option._id === soundSelect);
+    const validSettingSelect = (entity as any)[settingName].options.find((option: any) => {
+      return option._id === selected;
+    });
 
-    if (validSoundSelect) {
+    if (validSettingSelect) {
       await storage?.put("select", {
-        settingName: "sound",
+        settingName: settingName,
         isSync: false,
-        value: soundSelect,
+        value: selected,
+      });
+      SettingContext.setState(
+        produce((draft: any) => {
+          draft.entity[settingName].selected = selected;
+        })
+      );
+    } else {
+      SettingContext.setState((prevState) => ({
+        ...prevState,
+        status: "ERROR",
+        message: "Something is not right",
+      }));
+    }
+  }
+}
+
+export async function actionCreateThemeSetting(themeSetting: {
+  name: string;
+  value: {
+    primary: string;
+    secondary: string;
+    danger: string;
+  };
+}) {
+  const { status, entity, storage } = SettingContext.getState();
+
+  if (status === "READY") {
+    const validThemeSetting = entity.theme.options.find((option) => {
+      return option.name === themeSetting.name;
+    });
+
+    if (validThemeSetting) {
+      SettingContext.setState((prevState) => ({
+        ...prevState,
+        status: "ERROR",
+        message: "Tên chủ đề đã tồn tại",
+      }));
+    } else {
+      if (themeSetting.name) {
+        const id = uuidv4();
+
+        await storage?.add("options", {
+          _id: id,
+          isSync: false,
+          settingName: "theme",
+          ...themeSetting,
+        });
+
+        await storage?.put("select", { settingName: "theme", isSync: false, value: id });
+
+        SettingContext.setState(
+          produce((draft) => {
+            draft.message = "Thêm chủ đề mới thành công";
+            draft.entity.theme.selected = id;
+            draft.entity.theme.options.push({
+              _id: id,
+              isDefault: false,
+              ...themeSetting,
+            });
+          })
+        );
+      } else {
+        SettingContext.setState((prevState) => ({
+          ...prevState,
+          status: "ERROR",
+          message: "Tên chủ đề không hợp lệ",
+        }));
+      }
+    }
+  }
+}
+
+export async function actionUpdateTheme(
+  themeId: string,
+  themeSetting: {
+    name: string;
+    value: {
+      primary: string;
+      secondary: string;
+      danger: string;
+    };
+  }
+) {
+  const { status, entity, storage } = SettingContext.getState();
+
+  if (status === "READY") {
+    const validThemeSetting = entity.theme.options.find((option) => {
+      return option._id !== themeId && option.name === themeSetting.name;
+    });
+
+    if (validThemeSetting) {
+      SettingContext.setState((prevState) => ({
+        ...prevState,
+        status: "ERROR",
+        message: "Tên chủ đề đã tồn tại",
+      }));
+    } else {
+      if (themeSetting.name) {
+        await storage?.put("options", {
+          _id: themeId,
+          isSync: false,
+          settingName: "theme",
+          ...themeSetting,
+        });
+
+        SettingContext.setState(
+          produce((draft) => {
+            draft.message = "Cập nhật chủ đề thành công";
+            for (let index = 0; index < entity.theme.options.length; index++) {
+              if (entity.theme.options[index]._id === themeId) {
+                draft.entity.theme.options[index].name = themeSetting.name;
+                draft.entity.theme.options[index].value = themeSetting.value;
+                break;
+              }
+            }
+          })
+        );
+      } else {
+        SettingContext.setState((prevState) => ({
+          ...prevState,
+          status: "ERROR",
+          message: "Tên chủ đề không hợp lệ",
+        }));
+      }
+    }
+  }
+}
+
+export async function actionDeleteThemeSetting(themeId: string) {
+  const { status, entity, storage } = SettingContext.getState();
+
+  if (status === "READY") {
+    const validThemeSetting = entity.theme.options.find((option) => {
+      return option._id === themeId;
+    });
+
+    if (validThemeSetting) {
+      const { primaryDefault } = entity.theme;
+
+      await storage?.delete("options", themeId);
+      await storage?.put("select", {
+        settingName: "theme",
+        isSync: false,
+        value: primaryDefault,
       });
 
       SettingContext.setState(
         produce((draft) => {
-          draft.entity.sound.selected = soundSelect;
+          draft.message = "Xoá chủ đề thành công";
+          draft.entity.theme.selected = primaryDefault;
+          for (let index = 0; index < entity.theme.options.length; index++) {
+            if (entity.theme.options[index]._id === themeId) {
+              draft.entity.theme.options.splice(index, 1);
+              break;
+            }
+          }
         })
       );
     } else {
-      SettingContext.setState(
-        produce((draft) => {
-          draft.status = "ERROR";
-          draft.errorMessage = "Something is not right";
-        })
-      );
+      SettingContext.setState((prevState) => ({
+        ...prevState,
+        status: "ERROR",
+        message: "Something is not right",
+      }));
     }
   }
 }
