0e102218ea3074b39113396cb05cd1ad5e198097
thaihoanganh <hoanganh0811999@gmail.com>
2021-08-20 16:14:06 +0700

    refactor(button-component): add danger color

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -12,10 +12,18 @@ const COLORS = {
     "bg-contrast-secondary bg-opacity-0 hover:bg-opacity-8 active:bg-opacity-32",
     "text-contrast-secondary",
   ],
+  danger: [
+    "bg-danger bg-opacity-12",
+    "bg-contrast-danger bg-opacity-0 hover:bg-opacity-16 active:bg-opacity-32",
+    "text-danger",
+  ],
 };
 
 export interface ButtonProps
-  extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
   color?: keyof typeof COLORS;
   fullWitdh?: boolean;
 }
@@ -39,7 +47,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
           className={classNames(
             "text-button font-medium",
             COLORS[color][2],
-            disabled && color === "secondary" && "text-opacity-32"
+            disabled && (color === "secondary" || color === "danger") && "text-opacity-32"
           )}
         >
           {children}
