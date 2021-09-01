35f81b2485b70899fbef6a6bc86abdb2f81853f7
thaihoanganh <hoanganh0811999@gmail.com>
2021-09-01 16:45:46 +0700

    refactor(MainLayout-component): udpate style Mainlayout

--- src/components/templates/MainLayout/MainLayout.tsx
+++ src/components/templates/MainLayout/MainLayout.tsx
@@ -9,9 +9,9 @@ const MainLayout: React.FC<MainLayoutProps> = ({ header, content }) => {
 	return (
 		<div className="min-h-screen bg-secondary">
 			<div className="h-56">
-				<div className="desktop:w-960 h-full mx-auto p-md">{header}</div>
+				<div className="desktop:w-960 h-full mx-auto">{header}</div>
 			</div>
-			<div className="desktop:w-960 mx-auto p-md">{content}</div>
+			<div className="desktop:w-960 mx-auto">{content}</div>
 		</div>
 	);
 };
